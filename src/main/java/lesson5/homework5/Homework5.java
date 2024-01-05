a8b125b025486f8f6dc8bcd11dd8e9d47f1a5e2f
Nikita Robsky <kalipso,get-chief@yandex.ru>
2024-01-05 19:09:00 +0300

    init:homework5 - multithreading

---
+++ src/main/java/lesson5/homework5/Homework5.java
@@ -0,0 +1,24 @@
+package main.java.lesson5.homework5;
+
+public class Homework5 extends Thread {
+    public void run() {
+
+        Philosopher philosopher1 = new Philosopher("Philosopher-1");
+        Philosopher philosopher2 = new Philosopher("Philosopher-2");
+        Philosopher philosopher3 = new Philosopher("Philosopher-3");
+        Philosopher philosopher4 = new Philosopher("Philosopher-4");
+        Philosopher philosopher5 = new Philosopher("Philosopher-5");
+
+        try {
+            philosopher1.join();
+            philosopher2.join();
+            philosopher3.join();
+            philosopher4.join();
+            philosopher5.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Все философы поели и подумали");
+    }
+}
---
+++ src/main/java/lesson5/homework5/Philosopher.java
@@ -0,0 +1,18 @@
+package main.java.lesson5.homework5;
+
+public class Philosopher extends Thread {
+
+    private final String name;
+
+    public Philosopher(String name) {
+        this.name = name;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        Thread.currentThread().setName(name);
+        Dinner.dinner();
+    }
+
+}
